ad717e146d41c37f948d4f32dd4b21d4f160d593
Ajay Singh <ajaysingh942@gmail.com>
2023-04-03 12:13:29 +0530

    revision done till hero detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-revision-of-heroes';
+  title = 'Angular Heroes HomeComing!';
 }
