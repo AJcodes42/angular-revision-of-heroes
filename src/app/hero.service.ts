ce527e78c41c5aa2f5da6fabfc63a1296302801f
Ajay Singh <ajaysingh942@gmail.com>
2023-04-05 00:38:19 +0530

    Observable,Subject and http handling done

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,24 +1,106 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable,of } from 'rxjs';
+import { Observable,of,catchError,map,tap } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
 
-  constructor(private messageService : MessageService) { }
+  httpOptions = {
+    headers : new HttpHeaders({ 'Content-Type': 'application/json'})
+  };
+
+  constructor(private httpClient : HttpClient,
+     private messageService : MessageService) { }
 
   getHeroes() : Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    // const heroes = of(HEROES);
+    // this.messageService.add('HeroService: fetched heroes');
+    // return heroes;
+    
+    return this.httpClient.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes',[]))
+    );
   }
 
   getHero(id : number) : Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!; // ! explanation https://stackoverflow.com/questions/43679230/typescript-whats-the-meaning-of-a-at-the-end-of-an-attribution
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    // const hero = HEROES.find(h => h.id === id)!; // ! explanation https://stackoverflow.com/questions/43679230/typescript-whats-the-meaning-of-a-at-the-end-of-an-attribution
+    // this.messageService.add(`HeroService: fetched hero id=${id}`);
+    // return of(hero);
+    
+    const url = `${this.heroesUrl}/${id}`;
+    return this.httpClient.get<Hero>(url).pipe(
+    tap(_ => this.log(`fetched hero id=${id}`)),
+    catchError(this.handleError<Hero>(`getHero id=${id}`))
+  );
   }
+
+  private log(message: String) : void {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  updateHero(hero : Hero) : Observable<any> {
+    return this.httpClient.put(this.heroesUrl,hero,this.httpOptions)
+    .pipe(tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.httpClient.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.httpClient.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+         this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }
